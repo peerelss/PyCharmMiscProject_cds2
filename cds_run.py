c2ff42865147e0c554bec0648eaecb400c79cb0f
cdstx <xiepeerless@gmail.com>
2025-12-17 14:40:51 -0600

    add requiremen

--- cds_run.py
+++ cds_run.py
@@ -1,16 +1,27 @@
 import ob_monitor
 import utils.utils_k
+from mienr_info import get_miner_temp_by_ip, get_miner_hash_rate_by_ip
 from utils.get_hour_power_price import get_ercot_hb_west_prices
 from utils.utils_k import ping_ip
 from telegram import Bot
 import asyncio
 import schedule
 import time
+from datetime import datetime
+
+now_local = datetime.now()
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["network_monitor"]
+collection = db["ip_offline_logs"]
+collection_over_heat = db["ip_over_heat"]
 
 HASH_RATE=200 # 默认算力
 ONLINE_MINER=1004 #默认在线机器
 
 BOT_TOKEN = "5802231356:AAGomB_cjbTKCNX4kDbnUykgRC2lGaI2GKk"
+
 CHAT_ID = "750326239"
 CHAT_ID_ZHANG_PENG = '8569953357'
 bot = Bot(token=BOT_TOKEN)
@@ -54,16 +65,51 @@ def get_higher_70_power_price():
     else:
         print("未能成功获取电价数据。")
 
+    if len(ip_over_heat) > 0:
+        message_str = "高温ip".join(ip_offline)
+
+        docs = [
+            {
+                "ip": ip,
+                "status": "over_heat",
+                "temp": ip_temp,
+                "created_at": datetime.now()
+            }
+            for ip, ip_temp in ip_over_heat
+        ]
+        collection_over_heat.insert_many(docs)
+
 
 def run_task_tele():
     ips = utils.utils_k.txt_2_list('ips.txt')
     ip_offline = []
+    ip_over_heat = []
+    time_now = datetime.now()
     for ip in ips:
         if not ping_ip(ip):
-            print(ip)
+            print(f"{ip},离线时间为:{time_now}")
             ip_offline.append(ip)
+        else:
+            ip_hash=get_miner_hash_rate_by_ip(ip)
+            if ip_hash and ip_hash>0:
+                pass
+            else:
+                temp_ip = get_miner_temp_by_ip(ip)
+                if temp_ip and temp_ip['temp'] > 70:
+                    ip_over_heat.append([ip, temp_ip['temp']])
+                    print(f"{ip} 高温,温度为：{temp_ip['temp']},时间为:{time_now}")
     if len(ip_offline) > 0:
         message_str = "离线ip".join(ip_offline)
+
+        docs = [
+            {
+                "ip": ip,
+                "status": "offline",
+                "created_at": datetime.now()
+            }
+            for ip in ip_offline
+        ]
+        collection.insert_many(docs)
         if len(message_str) < 4000:
             asyncio.run(send_tele(message_str))
         else:
--- mienr_info.py
+++ mienr_info.py
@@ -1,8 +1,72 @@
 import socket
 import json
-from logging import exception
+import re
 
-from utils.utils_k import ping_ip
+
+def split_json_objects(text):
+    objs = []
+    buf = ""
+    depth = 0
+
+    for ch in text:
+        if ch == "{":
+            depth += 1
+        if depth > 0:
+            buf += ch
+        if ch == "}":
+            depth -= 1
+            if depth == 0:
+                objs.append(buf)
+                buf = ""
+
+    return objs
+
+
+def merge_cgminer_json(text):
+    result = {}
+
+    for js in split_json_objects(text):
+        try:
+            obj = json.loads(js)
+            result.update(obj)
+        except json.JSONDecodeError:
+            pass
+
+    return result
+
+
+def cgminer_status(ip, timeout=1.0):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((ip, 4028))
+
+        payload = json.dumps({"command": "stats"}).encode()
+        s.sendall(payload)
+
+        chunks = []
+        while True:
+            try:
+                data = s.recv(4096)
+                if not data:
+                    break
+                chunks.append(data)
+            except socket.timeout:
+                break
+
+        s.close()
+
+        raw = b"".join(chunks)
+        text = raw.decode("utf-8", errors="ignore")
+
+        # 去 NULL
+        text = text.replace("\x00", "")
+
+        # 拆分并合并多个 JSON
+        return merge_cgminer_json(text)
+
+    except Exception:
+        return None
 
 
 def cgminer_summary(ip, timeout=1.0):
@@ -16,7 +80,6 @@ def cgminer_summary(ip, timeout=1.0):
 
         raw = s.recv(4096)
         s.close()
-
 
         text = raw.decode("utf-8", errors="ignore")
 
@@ -35,36 +98,46 @@ def cgminer_summary(ip, timeout=1.0):
 
     except Exception as e:
         print(f"访问 {ip} 时出错: {e}")
-        print("原始返回：", raw)
         return None
+
+
 # ------------------ 测试 ------------------
 
 def get_miner_hash_rate_by_ip(ip):
-    try:
-        if ping_ip(ip):
-            miner_info=cgminer_summary(ip)
-            if miner_info is None:
-                return None
-            else:
-                return ip, {
-                    "code": 0,
-                    "status": "normal",
-                    "hashrate": miner_info["SUMMARY"][0]["MHS 30s"]
-                }
-        else:
-            return ip, {
-                "code": -1,
-                "status": "offline",
-                "hashrate": 0
-            }
-    except Exception as e:
-        print(e)
-        return ip, {
-            "code": -1,
-            "status": "error",
-            "hashrate": 0
-        }
+    miner_info = cgminer_summary(ip)
+    if miner_info is None:
+        return None
+    else:
+        return miner_info["SUMMARY"][0]["MHS av"]
+
+
+def parse_temp_info(text: str):
+    pattern = r"Temp\[(\d+)\]\s+TMax\[(\d+)\]\s+TAvg\[(\d+)\]"
+    match = re.search(pattern, text)
+
+    if not match:
+        return None
+
+    return {
+        "temp": int(match.group(1)),
+        "tmax": int(match.group(2)),
+        "tavg": int(match.group(3))
+    }
+
+
+def get_miner_temp_by_ip(ip):
+    miner_info = cgminer_status(ip)
+    if miner_info is None:
+        return None
+    else:
+        try:
+            temp_str = miner_info["STATS"][0]['MM ID0']
+            info = parse_temp_info(temp_str)
+            return (info)
+        except Exception as e:
+            return None
+
 
 if __name__ == "__main__":
-    h= get_miner_hash_rate_by_ip('10.1.1.1')
-    print(h)
+    # get_miner_hash_rate_by_ip('10.3.1.75')
+    print(get_miner_temp_by_ip('10.3.1.75'))
